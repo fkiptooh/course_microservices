f54a9addee78a9279cb4256816b4eda9af0cfb9d
fkiptooh <fkiptooh@gmail.com>
2022-12-24 20:31:23 +0300

    Readme Updated

--- PurchaseMicroservice/src/main/java/com/example/purchasemicroservice/security/SecurityConfig.java
+++ PurchaseMicroservice/src/main/java/com/example/purchasemicroservice/security/SecurityConfig.java
@@ -1,2 +1,53 @@
-package com.example.purchasemicroservice.security;public class SecurityConfig {
+package com.example.purchasemicroservice.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig
+{
+    @Value("${service.security.secure-key-username}")
+    private String SECURE_KEY_USERNAME;
+
+    @Value("${service.security.secure-key-password}")
+    private String SECURE_KEY_PASSWORD;
+    @Bean
+    public PasswordEncoder encoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager()
+    {
+        UserDetails user = User.builder()
+                .username(SECURE_KEY_USERNAME)
+                .password(encoder().encode(SECURE_KEY_PASSWORD))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+    {
+        return http.csrf().disable().
+                headers().frameOptions().disable().
+                and().
+                httpBasic(Customizer.withDefaults())
+                .build();
+    }
 }
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 -This is a backend course management system with four microservices
 1. Course Microservice.
 2. Purchase Microservice.
+3. Api-Gateway Microservice to manage user authentication and authorization
+4. Eureka server which applications register to and also to track microservices when they are up or down.
